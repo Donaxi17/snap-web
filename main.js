fd55b3bec321ea190b82b2fa1e5f203d870e9472
Donaxi <donaxjimenez00@gmail.com>
2023-08-10 15:01:31 -0500

    finalisacion

--- main.js
+++ main.js
@@ -5,22 +5,29 @@ var contenFeatures = document.getElementById("conten__features");
 var lblCompany = document.getElementById("lbl__company");
 var contenCompany = document.getElementById("conten__company");
 
+var imgFeatures = document.getElementById("img__features");
+var imgCompany = document.getElementById("img__company");
+
 lblFeature.addEventListener("click", () => {
     if (contenFeatures.style.display === 'none') {
         // Si el elemento está oculto, mostrarlo
         contenFeatures.style.display = 'block';
+        imgFeatures.src = "./images/icon-arrow-up.svg";
     } 
     else{
         // Si el elemento está visible, ocultarlo
         contenFeatures.style.display = 'none';
+        imgFeatures.src = "./images/icon-arrow-down.svg";
     }
 })
 
 lblCompany.addEventListener("click", () => {
     if (contenCompany.style.display === 'none') {
         contenCompany.style.display = 'block';
+        imgCompany.src = "./images/icon-arrow-up.svg";
     } else {
         contenCompany.style.display = 'none';
+        imgCompany.src = "./images/icon-arrow-down.svg";
     }
 })
 
